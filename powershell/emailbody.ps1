2c887f6f16699e3e9dc0d600493a58ff9771512c
21ahart <aidenhartranft>
2022-09-15 20:56:36 -0400

    powershell files

---
+++ powershell/emailbody.ps1
@@ -0,0 +1,11 @@
+﻿
+$IP =  (get-netipaddress).ipv4address | Select-String "192*"
+$USER = $env:USERNAME
+$HOSTNAME = hostname
+$VERMA = $HOST.Version.Major
+$DAY = (Get-Date).DayOfWeek
+$DATEDAY = (Get-Date).Day
+$DATEMON = (get-date).Month
+$DATEYEA = (get-date).Year
+
+$BODY = "This Machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. PowerShell Version $VERMA. Today's Date is $DAY, $DATEMON $DATEDAY $DATEYEA"
