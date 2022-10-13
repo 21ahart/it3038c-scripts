46a35ccbdf793c7831afe8fc538f8d0bf678a4e9
21ahart <aidenhartranft>
2022-10-13 15:41:37 -0400

    Lab 7

---
+++ labs/modules.ps1
@@ -0,0 +1,15 @@
+﻿function fileinfo($str)
+{
+    $version = Get-FileVersionInfo $str
+    $tail = Get-FileTail $str
+    $hash = get-hash $str
+
+    Write-Output "***The file version of $str is:***"
+    Write-Output $version
+    Write-Output "***The last few lines of the file are:***"
+    Write-Output $tail
+    Write-Output "***The hash for the file is:***"
+    Write-Output $hash
+}
+$file = read-host "Enter the file you want to look at"
+fileinfo($file)
